1d1ddd6106533a8d4969d2c9144c46493733db85
Intron7 <severin.dicks@icloud.com>
2025-05-27 11:30:56 +0200

    test

--- src/decoupler/pl/_network.py
+++ src/decoupler/pl/_network.py
@@ -1,11 +1,11 @@
-from typing import Tuple
-
-import pandas as pd
-import numpy as np
 import matplotlib
+import matplotlib.cm
+import matplotlib.colors
+import matplotlib.gridspec
+import networkx as nx
+import numpy as np
+import pandas as pd
 from matplotlib.lines import Line2D
-import matplotlib.pyplot as plt
-import igraph as ig
 
 from decoupler._docs import docs
 from decoupler._Plotter import Plotter
@@ -16,8 +16,7 @@ def _src_idxs(
     sources: int | list | str,
     by_abs: bool,
 ) -> np.ndarray:
-    assert isinstance(sources, (int, list, str)), \
-    'sources must be int, list or str'
+    assert isinstance(sources, (int, list, str)), "sources must be int, list or str"
     if isinstance(sources, int):
         if by_abs:
             s_idx = np.argsort(-abs(score.values[0]))[:sources]
@@ -36,54 +35,54 @@ def _trg_idxs(
     targets: int | list | str,
     by_abs: bool,
 ) -> np.ndarray:
-    assert isinstance(targets, (int, list, str)), \
-    'targets must be int, list or str'
+    assert isinstance(targets, (int, list, str)), "targets must be int, list or str"
     if isinstance(targets, int):
-        net['prod'] = [data.iloc[0][t] * w if t in data.columns else 0 for t, w in zip(net['target'], net['weight'])]
+        net["prod"] = [
+            data.iloc[0][t] * w if t in data.columns else 0 for t, w in zip(net["target"], net["weight"], strict=False)
+        ]
         if by_abs:
-            net['prod'] = abs(net['prod'])
+            net["prod"] = abs(net["prod"])
         t_idx = (
-            net
-            .sort_values(['source', 'prod'], ascending=[True, False])
-            .groupby(['source'], observed=True)
+            net.sort_values(["source", "prod"], ascending=[True, False])
+            .groupby(["source"], observed=True)
             .head(targets)
-            .index
-            .values
+            .index.values
         )
     elif isinstance(targets, list):
-        t_idx = np.isin(net['target'].astype(str), targets)
+        t_idx = np.isin(net["target"].astype(str), targets)
     else:
-        t_idx = np.isin(net['target'].astype(str), [targets])
+        t_idx = np.isin(net["target"].astype(str), [targets])
     return t_idx
 
-    
+
 def _filter(
     data: pd.DataFrame,
     score: pd.DataFrame,
     net: pd.DataFrame,
-    sources: int,
-    targets: int,
+    sources: int | list | str,
+    targets: int | list | str,
     by_abs: bool,
-) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-    assert isinstance(data, pd.DataFrame), 'data must be pd.DataFrame'
-    assert isinstance(score, pd.DataFrame), 'score must be pd.DataFrame'
-    assert np.all(data.index == score.index) and (data.index.size == 1), \
-    'data and score need to have the same row index.'
-    assert isinstance(by_abs, bool), 'by_abs must be bool'
+) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    assert isinstance(data, pd.DataFrame), "data must be pd.DataFrame"
+    assert isinstance(score, pd.DataFrame), "score must be pd.DataFrame"
+    assert np.all(data.index == score.index) and (data.index.size == 1), (
+        "data and score need to have the same row index."
+    )
+    assert isinstance(by_abs, bool), "by_abs must be bool"
     # Select top sources
     s_idx = _src_idxs(score=score, sources=sources, by_abs=by_abs)
     # Filter
     score = score.iloc[:, s_idx]
-    net = net.loc[np.isin(net['source'].astype(str), score.columns.astype(str)), :].copy()
-    if 'weight' not in net.columns:
-        net['weight'] = 1.
+    net = net.loc[np.isin(net["source"].astype(str), score.columns.astype(str)), :].copy()
+    if "weight" not in net.columns:
+        net["weight"] = 1.0
     # Select top targets
     t_idx = _trg_idxs(data=data, net=net, targets=targets, by_abs=by_abs)
     # Filter
     net = net.loc[t_idx]
     # Filter unmatched features
-    data = data.loc[:, np.isin(data.columns.astype(str), net['target'].astype(str))]
-    net = net.loc[np.isin(net['target'].astype(str), data.columns.astype(str)), :]
+    data = data.loc[:, np.isin(data.columns.astype(str), net["target"].astype(str))]
+    net = net.loc[np.isin(net["target"].astype(str), data.columns.astype(str)), :]
     return data, score, net
 
 
@@ -91,7 +90,7 @@ def _norm(
     x: np.ndarray,
     vcenter: bool,
 ) -> matplotlib.colors.Normalize:
-    assert isinstance(vcenter, bool), 'vcenter must be bool'
+    assert isinstance(vcenter, bool), "vcenter must be bool"
     if vcenter:
         vmax = np.max(np.abs(x))
         norm = matplotlib.colors.Normalize(vmin=-vmax, vmax=vmax)
@@ -105,7 +104,7 @@ def _norm(
 def _dict_types(
     data: pd.DataFrame,
     score: pd.DataFrame,
-) -> Tuple[dict, np.ndarray]:
+) -> tuple[dict, np.ndarray]:
     vs = np.unique(np.hstack([data.columns, score.columns]))
     v_dict = {k: i for i, k in enumerate(vs)}
     types = (~np.isin(vs, score.columns)) * 1
@@ -118,7 +117,7 @@ def _net_2_elist(
 ) -> list:
     edges = []
     for i in net.index:
-        source, target = net.loc[i, 'source'], net.loc[i, 'target']
+        source, target = net.loc[i, "source"], net.loc[i, "target"]
         edge = [v_dict[source], v_dict[target]]
         edges.append(edge)
     return edges
@@ -128,26 +127,31 @@ def _net_2_g(
     data: pd.DataFrame,
     score: pd.DataFrame,
     net: pd.DataFrame,
-) -> ig.Graph:
+) -> nx.DiGraph:
     # Unify network
     v_dict, types = _dict_types(data=data, score=score)
     # Transform net to edges
     edges = _net_2_elist(net=net, v_dict=v_dict)
     # Create graph
-    g = ig.Graph(
-        edges=edges,
-        directed=True,
-    )
-    # Update attributes
-    g.es['weight'] = net['weight'].values
-    g.vs['type'] = types
-    g.vs['label'] = list(v_dict.keys())
-    g.vs['shape'] = np.where(types, 'circle', 'square')
+    g = nx.DiGraph()
+    g.add_edges_from(edges)
+
+    # Update edge attributes
+    for i, edge in enumerate(edges):
+        g.edges[edge]["weight"] = net["weight"].values[i]
+
+    # Update node attributes
+    node_labels = list(v_dict.keys())
+    for i, node in enumerate(g.nodes()):
+        g.nodes[node]["type"] = types[i]
+        g.nodes[node]["label"] = node_labels[i]
+        g.nodes[node]["shape"] = "o" if types[i] else "s"  # circle for targets, square for sources
+
     return g
 
 
 def _gcolors(
-    g: ig.Graph,
+    g: nx.DiGraph,
     data: pd.DataFrame,
     score: pd.DataFrame,
     s_norm: matplotlib.colors.Normalize,
@@ -157,41 +161,41 @@ def _gcolors(
 ) -> bool:
     cmaps = matplotlib.colormaps.keys()
     if (s_cmap in cmaps) and (t_cmap in cmaps):
-        s_cmap = matplotlib.colormaps.get_cmap(s_cmap)
-        t_cmap = matplotlib.colormaps.get_cmap(t_cmap)
-        color = []
-        for i, k in enumerate(g.vs['label']):
-            if g.vs['type'][i]:
-                color.append(t_cmap(t_norm(data[k].values[0])))
+        s_cmap_obj = matplotlib.colormaps.get_cmap(s_cmap)
+        t_cmap_obj = matplotlib.colormaps.get_cmap(t_cmap)
+        for node in g.nodes():
+            node_label = g.nodes[node]["label"]
+            if g.nodes[node]["type"]:
+                g.nodes[node]["color"] = t_cmap_obj(t_norm(data[node_label].values[0]))
             else:
-                color.append(s_cmap(s_norm(score[k].values[0])))
+                g.nodes[node]["color"] = s_cmap_obj(s_norm(score[node_label].values[0]))
         is_cmap = True
     else:
-        color = [s_cmap if typ == 0. else t_cmap for typ in g.vs['type']]
+        for node in g.nodes():
+            g.nodes[node]["color"] = s_cmap if g.nodes[node]["type"] == 0.0 else t_cmap
         is_cmap = False
-    g.vs['color'] = color
     return is_cmap
 
 
 @docs.dedent
 def network(
     net,
-    data: pd.DataFrame = None,
-    score: pd.DataFrame = None,
+    data: pd.DataFrame | None = None,
+    score: pd.DataFrame | None = None,
     sources: int | list | str = 5,
     targets: int | list | str = 10,
     by_abs=True,
     size_node=5,
     size_label=2.5,
-    s_cmap='RdBu_r',
-    t_cmap='viridis',
+    s_cmap="RdBu_r",
+    t_cmap="viridis",
     vcenter=False,
-    c_pos_w='darkgreen',
-    c_neg_w='darkred',
-    s_label='Enrichment\nscore',
-    t_label='Gene\nexpression',
-    layout='kk',
-    **kwargs
+    c_pos_w="darkgreen",
+    c_neg_w="darkred",
+    s_label="Enrichment\nscore",
+    t_label="Gene\nexpression",
+    layout="kk",
+    **kwargs,
 ):
     """
     Plot results of enrichment analysis as network.
@@ -231,14 +235,14 @@ def network(
         Layout to use to order the nodes. Check ``igraph`` documentation for more options.
     %(plot)s
     """
-    assert isinstance(net, pd.DataFrame), 'net must be pd.DataFrame'
-    assert (data is None) == (score is None), 'data and score must either both be None'
+    assert isinstance(net, pd.DataFrame), "net must be pd.DataFrame"
+    assert (data is None) == (score is None), "data and score must either both be None"
     if data is None:
-        srcs = net['source'].unique().astype('U')
-        score = pd.DataFrame(np.ones((1, srcs.size)), index=['0'], columns=srcs)
-        trgs = net['target'].unique().astype('U')
-        data = pd.DataFrame(np.ones((1, trgs.size)), index=['0'], columns=trgs)
-        t_cmap = 'white'
+        srcs = net["source"].unique().astype("U")
+        score = pd.DataFrame(np.ones((1, srcs.size)), index=["0"], columns=srcs)
+        trgs = net["target"].unique().astype("U")
+        data = pd.DataFrame(np.ones((1, trgs.size)), index=["0"], columns=trgs)
+        t_cmap = "white"
     # Filter
     fdata, fscore, fnet = _filter(
         data=data,
@@ -249,11 +253,15 @@ def network(
         by_abs=by_abs,
     )
     # Color norms
-    s_norm = _norm(x=fscore, vcenter=vcenter)
-    t_norm = _norm(x=fdata, vcenter=vcenter)
+    s_norm = _norm(x=fscore.values, vcenter=vcenter)
+    t_norm = _norm(x=fdata.values, vcenter=vcenter)
     # Make graph
     g = _net_2_g(data=fdata, score=fscore, net=fnet)
-    g.es['color'] = [c_pos_w if w > 0 else c_neg_w for w in g.es['weight']]
+    # Set edge colors
+    for edge in g.edges():
+        weight = g.edges[edge]["weight"]
+        g.edges[edge]["color"] = c_pos_w if weight > 0 else c_neg_w
+
     is_cmap = _gcolors(
         g=g,
         data=data,
@@ -264,23 +272,54 @@ def network(
         t_cmap=t_cmap,
     )
     # Instance
-    kwargs['ax'] = None
+    kwargs["ax"] = None
     bp = Plotter(**kwargs)
-    bp.fig.delaxes(bp.ax)
+    if bp.ax is not None:
+        bp.fig.delaxes(bp.ax)
     # Plot
     gs = matplotlib.gridspec.GridSpec(6, 3)
     ax1 = bp.fig.add_subplot(gs[:-1, :])
     ax2 = bp.fig.add_subplot(gs[-1, 0])
     ax3 = bp.fig.add_subplot(gs[-1, 1])
     ax4 = bp.fig.add_subplot(gs[-1, -1])
-    ig.plot(
-        g,
-        target=ax1,
-        layout=layout,
-        vertex_size=(size_node * bp.dpi) / (bp.figsize[0] * bp.figsize[0]),
-        vertex_size_label=(size_label * bp.dpi) / (bp.figsize[0] * bp.figsize[0]),
-        bbox_inches='tight',
-    )
+
+    # Create NetworkX layout
+    if layout == "kk":
+        pos = nx.kamada_kawai_layout(g)
+    elif layout == "spring":
+        pos = nx.spring_layout(g)
+    elif layout == "circular":
+        pos = nx.circular_layout(g)
+    else:
+        pos = nx.spring_layout(g)  # default fallback
+
+    # Extract node attributes for plotting
+    node_colors = [g.nodes[node]["color"] for node in g.nodes()]
+    node_shapes = [g.nodes[node]["shape"] for node in g.nodes()]
+    edge_colors = [g.edges[edge]["color"] for edge in g.edges()]
+    node_labels = {node: g.nodes[node]["label"] for node in g.nodes()}
+
+    # Plot nodes with different shapes
+    sources = [node for node in g.nodes() if g.nodes[node]["type"] == 0]
+    targets = [node for node in g.nodes() if g.nodes[node]["type"] == 1]
+
+    if sources:
+        source_colors = [g.nodes[node]["color"] for node in sources]
+        nx.draw_networkx_nodes(
+            g, pos, nodelist=sources, node_color=source_colors, node_shape="s", node_size=size_node * 100, ax=ax1
+        )
+
+    if targets:
+        target_colors = [g.nodes[node]["color"] for node in targets]
+        nx.draw_networkx_nodes(
+            g, pos, nodelist=targets, node_color=target_colors, node_shape="o", node_size=size_node * 100, ax=ax1
+        )
+
+    # Draw edges
+    nx.draw_networkx_edges(g, pos, edge_color=edge_colors, ax=ax1, arrows=True, arrowsize=20)
+
+    # Draw labels
+    nx.draw_networkx_labels(g, pos, labels=node_labels, font_size=size_label * 4, ax=ax1)
     if is_cmap:
         sm = matplotlib.cm.ScalarMappable(norm=s_norm, cmap=s_cmap)
         bp.fig.colorbar(sm, cax=ax2, orientation="horizontal", label=s_label)
@@ -290,19 +329,35 @@ def network(
         ax2.axis("off")
         ax4.axis("off")
     # Add legend
-    square = Line2D([0], [0], marker='s', color='w', label='Source', markerfacecolor='white',
-                    markeredgecolor='black', markersize=10)
-    circle = Line2D([0], [0], marker='o', color='w', label='Target', markerfacecolor='white',
-                    markeredgecolor='black', markersize=10)
-    line1 = Line2D((0, 0), (1, 0), color=c_pos_w, lw=2, marker='>',)
-    line2 = Line2D((0, 0), (1, 0), color=c_neg_w, lw=2, marker='>',)
+    square = Line2D(
+        [0], [0], marker="s", color="w", label="Source", markerfacecolor="white", markeredgecolor="black", markersize=10
+    )
+    circle = Line2D(
+        [0], [0], marker="o", color="w", label="Target", markerfacecolor="white", markeredgecolor="black", markersize=10
+    )
+    line1 = Line2D(
+        (0, 0),
+        (1, 0),
+        color=c_pos_w,
+        lw=2,
+        marker=">",
+    )
+    line2 = Line2D(
+        (0, 0),
+        (1, 0),
+        color=c_neg_w,
+        lw=2,
+        marker=">",
+    )
     handles = [square, circle, line1, line2]
-    labels = ['Source', 'Target', 'Positive', 'Negative']
+    labels = ["Source", "Target", "Positive", "Negative"]
     legend = ax3.legend(
         handles=[square, circle, line1, line2],
         labels=labels,
         frameon=False,
-        loc='center', bbox_to_anchor=(0.5, 0.5), bbox_transform=ax3.transAxes
+        loc="center",
+        bbox_to_anchor=(0.5, 0.5),
+        bbox_transform=ax3.transAxes,
     )
-    ax3.axis('off')
+    ax3.axis("off")
     return bp._return()
